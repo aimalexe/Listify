11ee6212890497af215483cc6517ed9ad2eac19c
aimalexe <aimalexe@gmail.com>
2023-03-02 11:35:35 +0500

    Some prepration for production

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require('config');
 
 require('./startups/connectDatabase')(); //? Connect with database
 require('./startups/routes')(app);  //? Endpoints are included from here.
+require('./startups/production')(app); //?m Middleware for production.
 
 const server = app.listen(config.get("PORT"), ()=>{
     console.info(`Listening on port ${config.get("PORT")} for ${config.get("environment")}`);
