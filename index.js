9a2f033337ea07b4dda37fa6b21968bcedeeb6ac
aimalexe <aimalexe@gmail.com>
2023-03-03 02:20:02 +0500

    merged production branch

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const winston = require('winston');
 require('./startups/logging')(); //? for logging
 require('./startups/connectDatabase')(); //? Connect with database
 require('./startups/routes')(app);  //? Endpoints are included from here.
+require('./startups/production')(app); //? Middleware for production.
 
 const server = app.listen(config.get("PORT"), ()=>{
     winston.info(`Listening on port ${config.get("PORT")} for ${config.get("environment")}`);
